569db270d4a6e9c14d6a63c230fe9123443df864
tperrinweembi <tperrin@weembi.com>
2020-10-22 22:41:48 +0200

    bug fix Joi requirement

--- models/rental.js
+++ models/rental.js
@@ -1,4 +1,4 @@
-const Joi = require("Joi");
+const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const mongoose = require("mongoose");
 const debug = require("debug")("app:all-dev");
